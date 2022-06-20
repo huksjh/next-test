927e68c251305fbb9acb45a0a658fb3c32205a44
huks <wkdgur1209@gmaillcom>
2022-06-20 23:00:16 +0900

    노션 api 연동 테스트

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,36 @@
 import Layout from "../components/layout";
+import { Client } from "@notionhq/client";
 
-export default function Projects() {
-    return (
-        <Layout>
-            <h1>프로젝트</h1>
-        </Layout>
-    );
+import { NOTION_DATABASE_ID, NOTION_DATABASE_SCREET } from "../config";
+
+export default function Projects({ projects }) {
+	console.log(projects);
+	return (
+		<Layout>
+			<h1>프로젝트</h1>
+		</Layout>
+	);
+}
+
+export async function getStaticProps() {
+	const options = {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Notion-Version": "2022-02-22",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${NOTION_DATABASE_SCREET}`,
+		},
+		body: JSON.stringify({ page_size: 100 }),
+	};
+
+	const res = await fetch(
+		`https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`,
+		options
+	);
+	const projects = await res.json();
+
+	return {
+		props: { projects }, // will be passed to the page component as props
+	};
 }
